f26cf357fec8c77ec5c862bca35b672e688123f7
iheyfiu <iheyn001@fiu.edu>
2020-09-27 08:28:31 -0400

    Reorganized/Added a player standin
    Put all environments in main scene folder. Added a circle with a movement script to server as "player".

---
+++ Graduation Simulator/Assets/PlayerStandin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerStandin : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        transform.position += movement * Time.deltaTime;
+    }
+}
